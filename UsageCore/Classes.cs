664dbf34922882b715f731a6d9bfadc7be9ea357
amularczyk <xniqux@gmail.com>
2018-04-30 11:38:41 +0200

    Adding project with usage DI in .NET Core.

---
+++ UsageCore/Classes.cs
@@ -0,0 +1,38 @@
+﻿namespace UsageCore
+{
+    public class SingletonClass : ISingletonClass
+    {
+    }
+
+    public interface ISingletonClass   
+    {
+    }
+
+    public class TransientClass : ITransientClass
+    {
+    }
+
+    public interface ITransientClass
+    {
+    }
+
+    public class ScopedClass : IScopedClass
+    {
+        public ScopedClass(ITransientClass transientClass)
+        {
+            
+        }
+    }
+
+    public interface IScopedClass
+    {
+    }
+
+    public class SampleClass : ISampleClass
+    {
+    }
+
+    public interface ISampleClass
+    {
+    }
+}
